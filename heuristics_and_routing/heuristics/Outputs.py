0a3ac0b1175f0d10449cef1988e3db4493b50ce8
Amasterman <alexandermasterman@gmail.com>
2023-10-26 14:28:33 +0100

    Changes to the documentaion adding the paramiters to the classes. Additionally began adding the meta classes for the dependancies

--- heuristics_and_routing/heuristics/Outputs.py
+++ heuristics_and_routing/heuristics/Outputs.py
@@ -6,3 +6,18 @@ Intended usage: If you are making changes here you are changing the outputs that
 the world enviornemnt.
 
 """
+
+
+class Outputs:
+    """
+    :param data_packet: When handling an output the log name/location is sent as a str pointing towards the file name/
+                        location
+    :type data_packet: str
+
+
+
+    """
+    def __init__(self, data_packet):
+
+        self.data_packet = data_packet
+        
